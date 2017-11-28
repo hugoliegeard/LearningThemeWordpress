98685aaa96961eb4f9bb08eb8c8a7b3d0222cd46
Hugo LIEGEARD <hugo@hl-media.co>
2017-11-28 10:28:06 +0100

    Dépot Initial de mon Thème

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Ici, je suis sur le fichier par défaut pour tout thème WP.
+     * Il est l'un des 2 OBLIGATOIRE (l'autre étant style.css)
+     * Il est utilisé pour afficher une page si aucun autre fichier n'est disponible
+     * https://developer.wordpress.org/themes/basics/template-hierarchy/
+     */
+
+    get_header();
+    get_footer();
+
+?>        
